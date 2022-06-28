6150551562d9fef7a91a4d90f0998c86d4a9fd61
Greg <gskril@gmail.com>
2022-06-28 17:19:50 -0400

    Add global favicon and sharing image

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
+import Head from 'next/head'
 
 const { chains, provider } = configureChains(
 	[
@@ -28,6 +29,13 @@ export default function App({ Component, pageProps }) {
 	return (
 		<WagmiConfig client={wagmiClient}>
 			<RainbowKitProvider chains={chains}>
+				<Head>
+					<meta property="og:image" content="https://ethnyc22.gregskril.com/sharing.png" />
+					<link rel="icon" type="image/png" sizes="128x128" href="/favicon-128.png" />
+					<link rel="icon" type="image/png" sizes="64x64" href="/favicon-64.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
+				</Head>
 				<Component {...pageProps} />
 			</RainbowKitProvider>
 		</WagmiConfig>
